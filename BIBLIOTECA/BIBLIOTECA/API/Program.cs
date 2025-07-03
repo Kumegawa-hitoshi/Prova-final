7c4fdf4575121ea06b4733765ab94616388ba8d8
Kumegawa-hitoshi <murilokumegawa@gmail.com>
2025-07-03 09:40:38 -0300

    implementando endpoint POST /api/livros com validações

--- BIBLIOTECA/BIBLIOTECA/API/Program.cs
+++ BIBLIOTECA/BIBLIOTECA/API/Program.cs
@@ -13,4 +13,29 @@ app.MapGet("/api/livros", (BibliotecaDbContext dbContext) =>
     return Results.Ok(livros);
 });
 
+app.MapPost("/api/livros", (BibliotecaDbContext dbContext, Livro livro) =>
+{
+    if (livro.Titulo?.Length < 3)
+    {
+        return Results.BadRequest("Título deve ter no mínimo 3 caracteres.");
+    }
+    if (livro.Autor?.Length < 3)
+    {
+        return Results.BadRequest("Autor deve ter no mínimo 3 caracteres.");
+    }
+    
+    var categoria = dbContext.Categorias.Find(livro.CategoriaId);
+    if (categoria == null)
+    {
+        return Results.NotFound("Categoria inválida. O ID da categoria fornecido não existe.");
+    }
+
+    livro.Categoria = categoria;
+    dbContext.Livros.Add(livro);
+    dbContext.SaveChanges();
+    
+    return Results.Created($"/api/livros/{livro.Id}", livro);
+});
+
+
 app.Run();
